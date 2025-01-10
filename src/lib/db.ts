6d53bd52d1b7a4030e271922730f2ef69ce6d6d5
unknown <haard.tripathi14@gmail.com>
2025-01-11 00:44:54 +0530

    mail done and db done

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,59 +1,59 @@
 
 
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+// const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env');
-}
+// if (!MONGODB_URI) {
+//     throw new Error('Please define the MONGODB_URI environment variable inside .env');
+// }
 
-let cached = (global as any).mongoose;
+// let cached = (global as any).mongoose;
 
-if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-            return mongoose;
-        });
-    }
-
-    try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
-
-    return cached.conn;
-}
-
-export default connectDB;
-
-// const mongoose = require("mongoose")
+// if (!cached) {
+//     cached = (global as any).mongoose = { conn: null, promise: null };
+// }
 
 // async function connectDB() {
-//     try {
-//         console.log("ABC")
-//         const conn = await mongoose.connect(process.env.MONGODB_URI)
-//         console.log(`MongoDB connected: ${conn.connection.host}`)
-//     } catch (error) {
-//         console.log("Mongodb error:", error)
+//     if (cached.conn) {
+//         return cached.conn;
 //     }
+
+//     if (!cached.promise) {
+//         const opts = {
+//             bufferCommands: false,
+//         };
+
+//         cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+//             return mongoose;
+//         });
+//     }
+
+//     try {
+//         cached.conn = await cached.promise;
+//     } catch (e) {
+//         cached.promise = null;
+//         throw e;
+//     }
+
+//     return cached.conn;
 // }
 
 // export default connectDB;
+
+const mongoose = require("mongoose")
+
+async function connectDB() {
+    try {
+        console.log("ABC")
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log("Mongodb error:", error)
+    }
+}
+
+export default connectDB;
 
 
 
